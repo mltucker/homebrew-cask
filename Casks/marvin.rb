9566a2ad1d76b5b56e66887d9cc01efddc299578
Mark Tucker <mark@amazingmarvin.com>
2023-06-30 00:13:59 +0200

    Add m1 build to marvin.

--- Casks/marvin.rb
+++ Casks/marvin.rb
@@ -1,8 +1,11 @@
 cask "marvin" do
+  arch arm: "-arm64", intel: ""
   version "1.63.0"
-  sha256 "8f6c4ca13956c41731086e2112fe79418e00248544e61d713b3ea9ff8d89c77a"
 
-  url "https://amazingmarvin.s3.amazonaws.com/Marvin-#{version}.dmg",
+  sha256 arm:   "0e0bc18c1274f3f6a2d45dd45ddd6448f0a917d8de5cd68a3a69eaabd0595903",
+         intel: "6fa673b815e3844844fc8ea5e4844997b7e96e54ffb3da0ebc16ad2bf20c2d24"
+
+  url "https://#{arch == "" ? "amazingmarvin" : "amarm"}.s3.amazonaws.com/Marvin-#{version}#{arch}.dmg",
       verified: "amazingmarvin.s3.amazonaws.com/"
   name "Amazing Marvin"
   desc "Personal productivity app"
